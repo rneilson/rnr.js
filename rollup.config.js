51169f5d4fe1871f16479cdc914045fe6540992d
Raymond Neilson <rneilson@interloper.ca>
2016-05-27 17:01:44 -0400

    Final tweaks to publish package

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,5 @@
-import json from 'rollup-plugin-json';
-
 export default {
 	entry: './src/main.js',
-	plugins: [
-		json(),
-	],
 	sourceMap: true,
 	moduleName: 'rnr',
 	banner: `/* @license
--- src/html.js
+++
@@ -1,3 +0,0 @@
-/* HTMLElement (and friends) manipulation functions */
-
-
